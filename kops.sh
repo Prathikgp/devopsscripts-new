1ac5af4b3baadc2a9842467ce68dcf6be70841ae
Prathikgp <prathikgp.1rn17me082@gmail.com>
2026-03-23 22:57:52 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -25,12 +25,13 @@ mv kops-linux-amd64 /usr/local/bin/kops
 aws s3api create-bucket --bucket prathik-kops-tstbkt12.k8s.local --region ap-south-1 --create-bucket-configuration LocationConstraint=ap-south-1
 aws s3api put-bucket-versioning --bucket prathik-kops-tstbkt12.k8s.local --region ap-south-1 --versioning-configuration Status=Enabled
 export KOPS_STATE_STORE=s3://prathik-kops-tstbkt12.k8s.local
-kops create cluster --name=prathik.k8s.local --zones=ap-south-1a,ap-south-1b --control-plane-count=1 --control-plane-size=t3.medium --node-count=2 --node-size=t3.small --node-volume-size=20 --control-plane-volume-size=20 --ssh-public-key=my-keypair.pub --image=ami-02d26659fd82cf299 --networking=calico --topology=public
+kops create cluster --name=prathik.k8s.local --zones=ap-south-1a,ap-south-1b --control-plane-count=1 --control-plane-size=t2.medium --node-count=2 --node-size=t2.micro 
+## --node-volume-size=20 --control-plane-volume-size=20 --ssh-public-key=my-keypair.pub --image=ami-02d26659fd82cf299 --networking=calico --topology=public
 kops update cluster --name prathik.k8s.local --yes --admin
 
 
 wq!
-
+      
 sh kops.sh
 
 export KOPS_STATE_STORE=s3://prathik-kops-tstbkt12.k8s.local
